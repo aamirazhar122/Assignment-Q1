299f7f7d39d029ab35a8c349e8947c7e2a005e0e
Muhammad Aamir Azhar <aamirlucky60@gmail.com>
2023-04-27 15:00:03 +0500

    Delate e41.js

--- e41.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-let magician_names = ["samuel", "Alic", "John", "Eric", "Boby"];
-show_magicians(magician_names);
-function show_magicians(magician) {
-    magician.forEach((magician) => console.log(magician));
-}
-;
